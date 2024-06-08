0f15dc241e7416858ae8a00a0a5c93a6d5afae42
Md Imran <samimdimran786@gmail.com>
2024-06-09 03:47:33 +0530

    naming conventions changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
@@ -19,14 +18,11 @@ const conn_str =
   "mongodb+srv://imran251099:imran251099@notescluster.luj719m.mongodb.net/Notes?retryWrites=true&w=majority&appName=NotesCluster";
 const client = new MongoClient(conn_str);
 client.connect();
+const notesDB = client.db("Notes");
+const notesCollection = notesDB.collection("users");
 
 app.get("/users", async (req, res) => {
-  // Find the first document in the collection
-  const db = client.db("Notes");
-  const collection = db.collection("users");
-
-  // Find the first document in the collection
-  const first = await collection.findOne();
+  const first = await notesCollection.findOne();
   res.send(first);
 });
 
