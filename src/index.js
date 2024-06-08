5a61eb72e5b3c1bcaa301c223dce9e0fddf378a3
Md Imran <samimdimran786@gmail.com>
2024-06-09 01:51:50 +0530

    db connection fixed

--- src/index.js
+++ src/index.js
@@ -11,22 +11,16 @@ app.get("/", (req, res) => {
 const conn_str =
   "mongodb+srv://imran251099:imran251099@notescluster.luj719m.mongodb.net/Notes?retryWrites=true&w=majority&appName=NotesCluster";
 const client = new MongoClient(conn_str);
-const notesDB = client.db("sample_mflix");
-const usersCollection = notesDB.collection("movies");
-async function run() {
-  try {
-    await client.connect();
-    console.log("Connected");
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.error);
+client.connect();
 
 app.get("/users", async (req, res) => {
   // Find the first document in the collection
-  const users = await usersCollection.find();
-  res.send(users);
+  const db = client.db("Notes");
+  const collection = db.collection("users");
+
+  // Find the first document in the collection
+  const first = await collection.findOne();
+  res.send(first);
 });
 
 app.listen(process.env.PORT || 8080);
